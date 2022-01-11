f4f1b2eb85d2075f92da790ae6baf22fe373a6c3
dangvh1 <97425597+dangvh1@users.noreply.github.com>
2022-01-11 17:43:54 +0700

    ex4

--- src/Java_Array/bai4.java
+++ src/Java_Array/bai4.java
@@ -41,11 +41,13 @@ public class bai4 {
         }else {
             for (int i = 2; i <= n/2  ; i++) {
                 if (n % i == 0){
-                    return false
+                    return false;
                 }
             }
             return true;
         }
 
     }
+
+
 }
